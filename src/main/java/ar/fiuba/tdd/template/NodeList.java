55f5eb6b54c912dae297d5554d6c36777823f0ab
adbonino <abonino88@gmail.com>
2016-08-31 14:04:33 -0300

    Adrian Bonino

---
+++ src/main/java/ar/fiuba/tdd/template/NodeList.java
@@ -0,0 +1,29 @@
+package ar.fiuba.tdd.template;
+
+
+class NodeList {
+
+    private Object item;
+    private NodeList nextNode;
+
+    NodeList(Object dato) {
+        this(dato, null);
+    }
+
+    private NodeList(Object dato, NodeList nodo) {
+        item = dato;
+        nextNode = nodo;
+    }
+
+    Object getItem() {
+        return item;
+    }
+
+    NodeList getNext_Node() {
+        return nextNode;
+    }
+
+
+
+
+}
