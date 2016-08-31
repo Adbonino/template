4f78e3e97f26728d3030b6d6732078388594d309
adbonino <abonino88@gmail.com>
2016-08-31 15:15:41 -0300

    Adrian Bonino

--- src/main/java/ar/fiuba/tdd/template/LinkedList.java
+++ src/main/java/ar/fiuba/tdd/template/LinkedList.java
@@ -2,21 +2,10 @@ package ar.fiuba.tdd.template;
 
 public class LinkedList {
 
-    private NodeList _firstNode;
-    private NodeList _lastNode;
-
-    public LinkedList() {
-        _firstNode = _lastNode = null ;
-    }
-
-    Object get_Item() {
-        return _firstNode.getItem();
-    }
 
 
-    boolean isClean() {
-        return _firstNode == null;
-    }
+
+
 
 
 
